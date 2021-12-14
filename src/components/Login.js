b59895d3f549c1f59650dc83728b8e839bc591bd
Ana Tulea <anatulea14@gmail.com>
2021-12-13 16:57:30 -0800

    added form validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,69 @@
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 import { Button, Form, Header } from "semantic-ui-react";
 
 const Login = () => {
   return (
-    <Form>
-      <Header as="h3">Login Form</Header>
-      <Form.Field>
-        <input placeholder="Email" />
-      </Form.Field>
-      <Form.Field>
-        <input placeholder="Password" />
-      </Form.Field>
-      <Button type="submit">Login</Button>
-      <Header as="h5">
-        Don't have an account? <Link to="/register">Register</Link>{" "}
-      </Header>
-    </Form>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <Header as="h3">Login Form</Header>
+          <Form.Field>
+            <input
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+          </Form.Field>
+
+          <Form.Field>
+            <input
+              placeholder="Password"
+              name="password"
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+          </Form.Field>
+
+          <Button type="submit">Login</Button>
+          <Header as="h5">
+            Don't have an account? <Link to="/register">Register</Link>{" "}
+          </Header>
+        </Form>
+      )}
+    </Formik>
   );
 };
+const SignupSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+});
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,27 +1,108 @@
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 import { Button, Form, Header } from "semantic-ui-react";
+
 const Register = () => {
   return (
-    <Form>
-      <Header as="h3">Register Form</Header>
-      <Form.Field>
-        <input placeholder="Username" />
-      </Form.Field>
-      <Form.Field>
-        <input placeholder="Email" />
-      </Form.Field>
-      <Form.Field>
-        <input placeholder="Password" />
-      </Form.Field>
-      <Form.Field>
-        <input placeholder="Confirm Password" />
-      </Form.Field>
-      <Button type="submit">Register</Button>
-      <Header as="h5">
-        Already have an account? <Link to="/login">Login</Link>{" "}
-      </Header>
-    </Form>
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={RegisterSchema}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <Header as="h3">Register Form</Header>
+          <Form.Field>
+            <input
+              placeholder="Username"
+              name="username"
+              type="text"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+            />
+            {errors.username && touched.username && errors.username}
+          </Form.Field>
+
+          <Form.Field>
+            <input
+              placeholder="Email"
+              name="email"
+              type="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+          </Form.Field>
+
+          <Form.Field>
+            <input
+              placeholder="Password"
+              name="password"
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+          </Form.Field>
+
+          <Form.Field>
+            <input
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              type="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.confirmPassword}
+            />
+            {errors.confirmPassword &&
+              touched.confirmPassword &&
+              errors.confirmPassword}
+          </Form.Field>
+
+          <Button type="submit">Register</Button>
+          <Header as="h5">
+            Already have an account? <Link to="/login">Login</Link>{" "}
+          </Header>
+        </Form>
+      )}
+    </Formik>
   );
 };
+const RegisterSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  confirmPassword: Yup.string().oneOf(
+    [Yup.ref("password"), null],
+    "Passwords must match"
+  ),
+});
 export default Register;
